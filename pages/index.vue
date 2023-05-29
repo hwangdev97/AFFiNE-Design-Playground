9760fadbe10160b40d2fc27445570289120f60dd
Hwang <hwangdev97@gmail.com>
2023-05-29 22:51:12 +0800

    style: add media query and responsive design

--- pages/index.vue
+++ pages/index.vue
@@ -27,7 +27,7 @@ const prototypeList = ref<ProtoTypeItem[]>([
         Some code/craft snippet for <span class="affineLogo"> </span> written by
         <span class="hwangLogo"> </span>
       </h1>
-      <h2><a href="https://affine.pro" >Try AFFiNE</a></h2>
+      <h2><a href="https://affine.pro">Try AFFiNE</a></h2>
     </section>
 
     <div class="prototypeGridLayout">
@@ -43,7 +43,11 @@ const prototypeList = ref<ProtoTypeItem[]>([
   height: 100vh;
   width: 100vw;
   background: #ffffff;
-  background-image: linear-gradient(rgba(0, 0, 0, 0.015) .1em, transparent .1em), linear-gradient(90deg, rgba(0, 0, 0, 0.015) .1em, transparent .1em);
+  background-image: linear-gradient(
+      rgba(0, 0, 0, 0.015) 0.1em,
+      transparent 0.1em
+    ),
+    linear-gradient(90deg, rgba(0, 0, 0, 0.015) 0.1em, transparent 0.1em);
   background-size: 1rem 1rem;
 }
 
@@ -70,7 +74,8 @@ const prototypeList = ref<ProtoTypeItem[]>([
   text-align: center;
 }
 
-.title h2, a {
+.title h2,
+a {
   display: flex;
   align-items: center;
   color: #585858;
@@ -97,5 +102,66 @@ const prototypeList = ref<ProtoTypeItem[]>([
   background-repeat: no-repeat;
   background-size: contain;
   margin: 0 0.5rem;
+}
+
+@media screen and (max-width: 1100px) { 
+  .prototypeGridLayout {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 20px;
+    padding: 3rem 6rem;
+  }
+
+  .title {
+    display: flex;
+    justify-content: space-between;
+    padding: 6rem 3rem 1rem 3rem ;
+    user-select: none;
+  }
+}
+
+@media screen and (max-width: 744px) {
+  .layout {
+    background-size: 0.5rem 0.5rem;
+  }
+
+  .prototypeGridLayout {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    grid-gap: 10px;
+    padding: 1rem 2rem;
+  }
+
+  .title {
+    display: flex;
+    justify-content: space-between;
+    padding: 0.85rem 1rem;
+    user-select: none;
+  }
+
+  .title h1 {
+    display: flex;
+    flex-direction: column;
+    font-size: 1.25rem;
+    text-align: left;
+  }
+
+  .title h2,
+  a {
+    font-size: 0.85rem;
+    text-align: center;
+  }
+
+  .affineLogo {
+    width: 24px;
+    height: 24px;
+    margin: 0 0.2rem;
+  }
+
+  .hwangLogo {
+    width: 24px;
+    height: 24px;
+    margin: 0 0.2rem;
+  }
 }
 </style>
